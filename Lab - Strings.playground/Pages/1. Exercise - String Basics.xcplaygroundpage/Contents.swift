eca2bd96288457b47d60b642e9436e087bbe9a6e
Kenneth Goff <kgoff1@live.maryville.edu>
2018-10-21 05:24:35 -0700

    Homework

--- Lab - Strings.playground/Pages/1. Exercise - String Basics.xcplaygroundpage/Contents.swift
+++ Lab - Strings.playground/Pages/1. Exercise - String Basics.xcplaygroundpage/Contents.swift
@@ -4,7 +4,7 @@
  Create a `name` constant and assign it a string literal representing your name.
  */
 
-
+let name = "Ken"
 /*:
  Create a `favoriteQuote` constant and assign it the following string literal:
  
@@ -12,12 +12,17 @@
  
  Write in your own favorite quote where indicated, and be sure to include escaped quotation marks. Print the value of `favoriteQuote`
  */
-
+let favoriteQuote = "My favorite quote is \"To be, or not to be, that is the question.\""
 
 /*:
  Write an if-else statement that prints "There's nothing here" if `emptyString` is empty, and "It's not as empty as I thought" otherwise.
  */
 let emptyString = ""
-
+if emptyString.isEmpty {
+    print("There's nothing here")
+    else if emptyString != emptyString {
+        print("It's not as empty as I thought")
+    }    }
+}
 
 //: page 1 of 5  |  [Next: Exercise - Concatenation and Interpolation](@next)
