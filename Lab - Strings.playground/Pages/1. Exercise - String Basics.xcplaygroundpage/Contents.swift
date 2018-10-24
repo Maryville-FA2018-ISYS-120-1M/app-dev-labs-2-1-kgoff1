661a10282824fc1757f15d39ba4b04ae799b71cf
Shanae Ullman <shanae.ullman@gmail.com>
2018-10-24 13:47:03 -0500

    SU edits

--- Lab - Strings.playground/Pages/1. Exercise - String Basics.xcplaygroundpage/Contents.swift
+++ Lab - Strings.playground/Pages/1. Exercise - String Basics.xcplaygroundpage/Contents.swift
@@ -13,14 +13,14 @@ let name = "Ken"
  Write in your own favorite quote where indicated, and be sure to include escaped quotation marks. Print the value of `favoriteQuote`
  */
 let favoriteQuote = "My favorite quote is \"To be, or not to be, that is the question.\""
-
+// missing print function, i.e.  print(favoriteQuote)
 /*:
  Write an if-else statement that prints "There's nothing here" if `emptyString` is empty, and "It's not as empty as I thought" otherwise.
  */
 let emptyString = ""
 if emptyString.isEmpty {
     print("There's nothing here")
-    else if emptyString != emptyString {
+    else if emptyString != emptyString { // Correct syntax is 'else' not 'else if'
         print("It's not as empty as I thought")
     }    }
 }
